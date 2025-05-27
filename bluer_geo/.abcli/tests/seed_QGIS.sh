9ce998de3d35ef8fd0225dff23ca33a0eb9dd6e5
kamangir <arash@kamangir.net>
2025-05-27 14:35:53 +0330

    ++ - kamangir/bolt#746

--- bluer_geo/.abcli/tests/seed_QGIS.sh
+++ bluer_geo/.abcli/tests/seed_QGIS.sh
@@ -3,8 +3,10 @@
 function test_bluer_geo_seed_QGIS() {
     local options=$1
 
-    bluer_ai_seed QGIS screen
+    bluer_ai_eval ,$options \
+        bluer_ai_seed QGIS screen
     [[ $? -ne 0 ]] && return 1
 
-    bluer_geo_QGIS seed screen
+    bluer_ai_eval ,$options \
+        bluer_geo_QGIS seed screen
 }
