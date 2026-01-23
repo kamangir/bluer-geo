6201fcbe09845d1659b554972a7f32a492f3cc25
kamangir <arash@kamangir.net>
2026-01-23 17:47:09 +0330

    pipeline fix, hopefully

--- bluer_geo/__init__.py
+++ bluer_geo/__init__.py
@@ -4,7 +4,7 @@ ICON = "🌐"
 
 DESCRIPTION = f"{ICON} AI for a Blue Planet."
 
-VERSION = "5.93.1"
+VERSION = "5.94.1"
 
 REPO_NAME = "bluer-geo"
 
--- bluer_geo/tests/test_env.py
+++ bluer_geo/tests/test_env.py
@@ -1,3 +1,4 @@
+from bluer_options.testing import are_01
 from bluer_ai.tests.test_env import test_bluer_ai_env
 from bluer_objects.tests.test_env import test_bluer_objects_env
 
@@ -44,4 +45,8 @@ def test_bluer_geo_env():
 
     assert env.BLUE_GEO_TEST_OBJECT
 
-    assert isinstance(env.BLUER_GEO_DISABLE_ALL_CATALOGS)
+    assert are_01(
+        [
+            env.BLUER_GEO_DISABLE_ALL_CATALOGS,
+        ]
+    )
